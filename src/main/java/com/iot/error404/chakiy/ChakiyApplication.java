075673974beca768ac36b8a97de4ea1b265706a5
GrandProgTest <u202124343@upc.edu.pe>
2025-04-20 19:58:33 -0500

    first commit

---
+++ src/main/java/com/iot/error404/chakiy/ChakiyApplication.java
@@ -0,0 +1,13 @@
+package com.iot.error404.chakiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChakiyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChakiyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/error404/chakiy/ChakiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.error404.chakiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChakiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
